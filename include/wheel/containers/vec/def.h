4a2293612ef9f4301ea567ac9cc3536d265f8539
Jens Pots <jens@jenspots.com>
2024-04-17 19:25:13 +0200

    refactor: updated vector `def.h` and `undef.h` structure

--- include/wheel/containers/vec/def.h
+++ include/wheel/containers/vec/def.h
@@ -1,14 +1,40 @@
+// clang-format off
+
 #include "wheel/misc/macros.h"
 
+// Conventions: base
 #define vec                LIBWHEEL_APPEND(vec)
+
+// Conventions: constructors and destructors
 #define vec_init           LIBWHEEL_DEFINE(vec, init)
 #define vec_init_ptr       LIBWHEEL_DEFINE(vec, init_ptr)
 #define vec_destroy        LIBWHEEL_DEFINE(vec, destroy)
 #define vec_destroy_ptr    LIBWHEEL_DEFINE(vec, destroy_ptr)
-#define vec_with_cap       LIBWHEEL_DEFINE(vec, with_cap)
-#define vec_pop            LIBWHEEL_DEFINE(vec, pop)
+
+// Conventions: retrieving elements
+#define vec_head           LIBWHEEL_DEFINE(vec, head)
+#define vec_head_ptr       LIBWHEEL_DEFINE(vec, head_ptr)
+#define vec_last           LIBWHEEL_DEFINE(vec, last)
+#define vec_last_ptr       LIBWHEEL_DEFINE(vec, last_ptr)
 #define vec_get            LIBWHEEL_DEFINE(vec, get)
 #define vec_get_ptr        LIBWHEEL_DEFINE(vec, get_ptr)
+
+// Conventions: serialization
+#define vec_serialize_json LIBWHEEL_DEFINE(vec, serialize_json)
+
+// Conventions: iterators
+#define vec_iter      LIBWHEEL_DEFINE(vec, iter)
+#define vec_begin     LIBWHEEL_DEFINE(vec, begin)
+#define vec_rbegin    LIBWHEEL_DEFINE(vec, rbegin)
+#define vec_end       LIBWHEEL_DEFINE(vec, end)
+#define vec_rend      LIBWHEEL_DEFINE(vec, rend)
+#define vec_iget      LIBWHEEL_DEFINE(vec, iget)
+#define vec_iget_ptr  LIBWHEEL_DEFINE(vec, iget_ptr)
+#define vec_next      LIBWHEEL_DEFINE(vec, next)
+
+// Additional definitions.
+#define vec_with_cap       LIBWHEEL_DEFINE(vec, with_cap)
+#define vec_pop            LIBWHEEL_DEFINE(vec, pop)
 #define vec_set            LIBWHEEL_DEFINE(vec, set)
 #define vec_grow           LIBWHEEL_DEFINE(vec, grow)
 #define vec_foreach        LIBWHEEL_DEFINE(vec, foreach)
@@ -18,13 +44,5 @@
 #define vec_filter         LIBWHEEL_DEFINE(vec, filter)
 #define vec_shallow_copy   LIBWHEEL_DEFINE(vec, shallow_copy)
 #define vec_deep_copy      LIBWHEEL_DEFINE(vec, deep_copy)
-#define vec_serialize_json LIBWHEEL_DEFINE(vec, serialize_json)
 
-#define vec_iter      LIBWHEEL_DEFINE(vec, iter)
-#define vec_begin     LIBWHEEL_DEFINE(vec, begin)
-#define vec_rbegin    LIBWHEEL_DEFINE(vec, rbegin)
-#define vec_end       LIBWHEEL_DEFINE(vec, end)
-#define vec_rend      LIBWHEEL_DEFINE(vec, rend)
-#define vec_iget      LIBWHEEL_DEFINE(vec, iget)
-#define vec_iget_ptr  LIBWHEEL_DEFINE(vec, iget_ptr)
-#define vec_next      LIBWHEEL_DEFINE(vec, next)
+// clang-format on
--- include/wheel/containers/vec/undef.h
+++ include/wheel/containers/vec/undef.h
@@ -1,12 +1,38 @@
+// clang-format off
+
+// Conventions: base
 #undef vec
+
+// Conventions: constructors and destructors.
 #undef vec_init
 #undef vec_init_ptr
 #undef vec_destroy
 #undef vec_destroy_ptr
-#undef vec_with_cap
-#undef vec_pop
+
+// Conventions: retrieving elements.
+#undef vec_head
+#undef vec_head_ptr
+#undef vec_last
+#undef vec_last_ptr
 #undef vec_get
 #undef vec_get_ptr
+
+// Conventions: serialization.
+#undef vec_serialize_json
+
+// Conventions: iterators.
+#undef vec_iter
+#undef vec_begin
+#undef vec_rbegin
+#undef vec_end
+#undef vec_rend
+#undef vec_iget
+#undef vec_iget_ptr
+#undef vec_next
+
+// Additional definitions.
+#undef vec_with_cap
+#undef vec_pop
 #undef vec_set
 #undef vec_grow
 #undef vec_foreach
@@ -16,13 +42,5 @@
 #undef vec_filter
 #undef vec_shallow_copy
 #undef vec_deep_copy
-#undef vec_serialize_json
 
-#undef vec_iter
-#undef vec_begin
-#undef vec_rbegin
-#undef vec_end
-#undef vec_rend
-#undef vec_iget
-#undef vec_iget_ptr
-#undef vec_next
+// clang-format on
